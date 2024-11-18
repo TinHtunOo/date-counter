d344686dd6045855f00ccec078ea2c82a95189b9
TinHtunOo <tinhtunhe@gmail.com>
2024-11-18 14:07:45 +0700

    date counter V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Step step={step} setStep={setStep} />
+      <Counter counter={counter} setCounter={setCounter} />
+      <Text step={step} counter={counter} />
     </div>
   );
 }
 
-export default App;
+function Step({ step, setStep }) {
+  function plusHandler() {
+    setStep((s) => s + 1);
+  }
+  function minusHandler() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  return (
+    <div className="step">
+      <button onClick={minusHandler}>-</button>
+      <span>Step: {step}</span>
+      <button onClick={plusHandler}>+</button>
+    </div>
+  );
+}
+
+function Counter({ counter, setCounter }) {
+  function plusHandler() {
+    setCounter((c) => c + 1);
+  }
+  function minusHandler() {
+    setCounter((c) => c - 1);
+  }
+  return (
+    <div className="counter">
+      <button onClick={minusHandler}>-</button>
+      <span>Count: {counter}</span>
+      <button onClick={plusHandler}>+</button>
+    </div>
+  );
+}
+
+function Text({ step, counter }) {
+  const currentDateInMS = Date.now();
+  const wantedDateInMS = currentDateInMS + step * counter * 86400000;
+  const wantedDate = new Date(wantedDateInMS)
+    .toString()
+    .split(" ", 4)
+    .join(" ");
+  // console.log(wantedDate);
+  // const week = wantedDate[0];
+  // const month = wantedDate[1];
+  // const day = wantedDate[2];
+  // const year = wantedDate[3];
+  return (
+    <p>
+      {currentDateInMS > wantedDateInMS
+        ? `${-step * counter} days ago was `
+        : ""}
+      {currentDateInMS < wantedDateInMS
+        ? `${step * counter} days from today is `
+        : ""}
+      {currentDateInMS === wantedDateInMS ? `Today was ` : ""}
+      {wantedDate}
+    </p>
+  );
+}
